832e5fdb6604197da8b1191c0a79c850b2f74bca
Eric Sayler <easayler@gmail.com>
2016-11-05 20:52:48 -0600

    reorganize app structure, add collision detection, bullet cooldown, stun, and update map and virtual platforms. add spritesheets

--- test/player-test.js
+++ test/player-test.js
@@ -23,59 +23,141 @@ describe('Player', function() {
     // let game = sinon.createStubInstance(Game);
 
     // var canvas = { fillRect: function() {} };
-    var game = { constructor: function() {} };
-
-    var spy = sinon.spy(game, "constructor");
-
-    var p1 = new Player(game, 1);
-    var p2 = new Player(game, 2);
+    // var game = { constructor: function() {} };
+    //
+    // var spy = sinon.spy(game, "constructor");
+    // var p1 = new Player({});
+    // var p2 = new Player({});
   });
 
   it('should have a constructor', function() {
-    expect(p1).to.be.a('Player');
-    p1.should.be.a('Player');
+    // var p1 = new Player({});
+
+    // expect(p1).to.be.a('Player');
+    assert.isFunction(Player);
+    // p1.should.be.a('Player');
     //;
   });
 
   context('on player creation', function() {
 
-    it.skip('should have an id', function() {
-      //;
+    it('should have an id', function() {
+      var p1 = new Player({});
+      p1.should.exist
+      p1.id.should.eql(1);
+
     });
-    it.skip('should have a property "x"', function() {
-      //;
+    it('should have a position "x" and "y"', function() {
+      var p1 = new Player({});
+      p1.should.exist;
+      p1.position.should.exist;
+      p1.position.should.include.keys('x');
+      p1.position.x.should.eql(0);
+      p1.position.should.include.keys('y');
+      p1.position.y.should.eql(0);
     });
-    it.skip('should have a property "y"', function() {
-      //;
+    it('should have an appropriate properties "width" and "height"', function() {
+      var p1 = new Player({});
+      p1.size.should.exist;
+      p1.size.should.include.keys('width');
+      p1.size.width.should.eql(20);
+      p1.size.should.include.keys('height');
+      p1.size.height.should.eql(20);
     });
-    it.skip('should have an appropriate property "width"', function() {
-      //;
+    it('should start with no velocity', function() {
+      var p1 = new Player({});
+      p1.velocity.should.exist;
+      p1.velocity.should.include.keys('x');
+      p1.velocity.x.should.eql(0);
+      p1.velocity.should.include.keys('y');
+      p1.velocity.y.should.eql(0);
     });
-    it.skip('should have an appropriate property "height"', function() {
-      //;
-    });
-    it.skip('should start with no velocity', function() {
-      //;
-    });
-    it.skip('should start with a speed', function() {
-      //;
+    it('should start with a speed', function() {
+      var p1 = new Player({});
+      p1.speed.should.exist;
+      p1.speed.should.be.at.least(2);
     });
   });
 
   context('while game is running', function() {
-    describe.skip('Player#jump()', function() {
-      it.skip('should be a function', function() {
-        //;
+    describe('Player#jump()', function() {
+      it('should be able to jump', function() {
+        var p1 = new Player({});
+        p1.jump.should.exist;
+        // expect(p1.jump()).to.change(p1, p1.position.y);
+        // p1.jump();
+        // p1.position.y.should.
       });
-      it.skip('should be able to jump', function() {
-        //;
-      });
-      it.skip('should be not be able to jump while jumping', function() {
-        //;
+      it('should be not be able to jump while jumping', function() {
+        var p1 = new Player({});
+        p1.jump.should.exist;
+        p1.jumping = true;
+        p1.jump().should.not.be.ok;
       });
       it.skip('should be not be able to jump repeatedly while holding down jump button', function() {
         //;
       });
     });
+
+    describe('Player#draw()', function() {
+      it.skip('should be drawn to the canvas', function() {
+        var p1 = new Player({});
+      });
+
+    });
+
+    describe('Player#left()', function() {
+      it('should be able to move left()', function() {
+        var p1 = new Player({position: { x: 100, y: 100}});
+        p1.left();
+        p1.position.x.should.decrease;
+
+      });
+
+    });
+
+    describe('Player#right()', function() {
+      it('should be able to move right', function() {
+        var p1 = new Player({});
+        var p1 = new Player({ position: { x: 100, y: 100} });
+        p1.right();
+        p1.position.x.should.increase;
+      });
+
+    });
+
+    describe('Player#shoot()', function() {
+      it.skip('should be able to shoot bullets', function() {
+        var g = new Game({});
+        var p1 = new Player({});
+        p1.shoot();
+        g.bullets.should.have.length.of.at.least(1);
+
+      });
+
+      it.skip('should be not be able to shoot right after shooting a bullet', function() {
+        var p1 = new Player({});
+        p1.shoot();
+        //Sinon timer
+        p1.shoot();
+        //should not shoot a bullet
+
+      });
+    });
+
+    describe('Player#update()', function() {
+      it.skip('should update player position', function() {
+        var p1 = new Player({ position: { x: 100, y: 100} });
+        p1.update().should.change(p1, p1.position.y);
+        // p1.position.y.should.change;
+      });
+
+      it.skip('should prevent player from moving off the map', function() {
+        var p1 = new Player({});
+      });
+
+    });
+
+
   });
 });
