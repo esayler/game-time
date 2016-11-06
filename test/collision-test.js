f272b9a663617925a21d1bc7bccf0ac41934b9be
Eric Sayler <easayler@gmail.com>
2016-11-06 15:30:09 -0700

    fix music, add character sprite animations, start collision tests

---
+++ test/collision-test.js
@@ -0,0 +1,49 @@
+// import lolex from 'lolex';
+import sinon from 'sinon';
+import Player from '../lib/player'
+import Game from '../lib/game'
+import {
+  platformCollisionCheck,
+  playerCollisionCheck,
+  projectileHitCheck
+} from '../lib/collision'
+
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const should = require('chai').should();
+
+describe('Collision Manager', function() {
+  before(function() {
+    // // runs before all tests in this block
+
+
+  });
+
+  beforeEach(function() {
+    // runs before each test in this block
+    // var p1 = new Player({});
+    // var p2 = new Player({});
+  });
+
+  describe('platformCollisionCheck()', function() {
+
+
+  });
+
+  describe('playerCollisionCheck()', function() {
+
+
+  });
+
+  describe('projectileHitCheck()', function() {
+
+
+  });
+
+
+
+  context.skip('', function() {
+
+  }
+
+}
