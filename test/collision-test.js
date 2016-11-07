60fc3ba184eecd193def7dd8963e4412a06e0001
Eric Sayler <easayler@gmail.com>
2016-11-06 18:25:37 -0700

    add image preloader

--- test/collision-test.js
+++ test/collision-test.js
@@ -26,6 +26,62 @@ describe('Collision Manager', function() {
   });
 
   describe('platformCollisionCheck()', function() {
+    context('if passThrough is false', function() {
+      it('should stop a player from falling through a platform', function() {
+        let platforms = [new Platform ({ x: 100, y: 100, width: 100, height: 100, passThrough: false })];
+        let p1 = new Player({position: { x: 100, y: 100 }});
+        // p1.size = { width: 45, height: 65 };
+        platformCollisionCheck(p1, platforms);
+
+
+      });
+
+      it('should allow a player to pass through the bottom of a platform', function() {
+        let platforms = [new Platform ({ x: 100, y: 100, width: 100, height: 100, passThrough: false })];
+        let p1 = new Player({position: { x: 100, y: 100 }});
+
+      });
+
+      it('should allow a player to pass through the right side of a platform', function() {
+        let platforms = [new Platform ({ x: 100, y: 100, width: 100, height: 100, passThrough: false })];
+        let p1 = new Player({position: { x: 100, y: 100 }});
+
+      });
+
+      it('should allow a player to pass through the left side of a platform', function() {
+        let platforms = [new Platform ({ x: 100, y: 100, width: 100, height: 100, passThrough: false })];
+        let p1 = new Player({position: { x: 100, y: 100 }});
+
+      });
+
+    });
+
+
+    context('if passThrough is true', function() {
+      it('should stop a player from falling through a platform', function() {
+        let platforms = [new Platform ({ x: 100, y: 100, width: 100, height: 100, passThrough: true })];
+        let p1 = new Player({position: { x: 100, y: 100 }});
+
+      });
+
+      it('should not allow a player to pass through the bottom of a platform', function() {
+        let platforms = [new Platform ({ x: 100, y: 100, width: 100, height: 100, passThrough: true })];
+        let p1 = new Player({position: { x: 100, y: 100 }});
+
+      });
+
+      it('should not allow a player to pass through the right side of a platform', function() {
+        let platforms = [new Platform ({ x: 100, y: 100, width: 100, height: 100, passThrough: true })];
+        let p1 = new Player({position: { x: 100, y: 100 }});
+
+      });
+
+      it('should not allow a player to pass through the left side of a platform', function() {
+        let platforms = [new Platform ({ x: 100, y: 100, width: 100, height: 100, passThrough: true })];
+        let p1 = new Player({position: { x: 100, y: 100 }});
+
+      });
+    });
 
 
   });
