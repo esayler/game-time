9fbbe7f9ba391559e7793072778d5d763f14c088
Eric Sayler <easayler@gmail.com>
2017-03-09 11:20:17 -0500

    update audio paths

--- main.bundle.js
+++ main.bundle.js
@@ -1546,13 +1546,13 @@
 
 	var soundArray = [{
 	  label: 'happyMusic',
-	  src: './audio/happy.mp3'
+	  src: 'audio/happy.mp3'
 	}, {
 	  label: 'loadingLoop',
-	  src: './audio/loading-loop-0.wav'
+	  src: 'audio/loading-loop-0.wav'
 	}, {
 	  label: 'theLastEncounterExtended',
-	  src: './audio/tle-extended.mp3'
+	  src: 'audio/tle-extended.mp3'
 	}];
 
 	exports.imageArray = imageArray;
